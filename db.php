bc39465cdc093cce845991d300319fa69882ed3f
Abdul Haris J <abdulharisju03@gmail.com>
2024-10-07 09:09:43 +0700

    Update db.php

--- db.php
+++ db.php
@@ -11,4 +11,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
+
+    echo "Koneksi berhasil!";
 ?>
